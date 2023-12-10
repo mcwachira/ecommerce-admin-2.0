9b92a19eedb37eae64c36feaa07fe7caf2aa1e2c
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-12-10 09:36:19 +0300

    initialized the app and added shadcn ui as a component library

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello admin dashboard</h1>;
+      <Button size="default" variant="outline">
+        Click Me
+      </Button>
+    </div>
+  );
+}
