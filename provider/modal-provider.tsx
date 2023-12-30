97c79a6840eb14b460d8b3b9cc663c5859379189
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-12-30 12:55:07 +0300

    Update modal-provider.tsx

--- provider/modal-provider.tsx
+++ provider/modal-provider.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import StoreModal from "@/components/models/store-modal";
+import StoreModal from "@/components/modals/store-modal";
 import { useEffect, useState } from "react";
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
 
+  //prevent hydration error when opening the modal
   useEffect(() => {
     setIsMounted(true);
   }, []);
