57349a35b07d62994852adcdbbe22e24aa2db651
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-01-15 11:19:14 +0300

    Chore :Updated the orders component

--- app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -9,6 +9,7 @@ import React from "react";
 import {Order} from "@prisma/client";
 import {OrderColumn, columns} from "@/app/(dashboard)/[storeId]/(routes)/orders/components/columns";
 import {DataTable} from "@/components/ui/data-table";
+import {useRouter} from "next/navigation";
 
 
 interface OrderClientProps{
@@ -17,27 +18,14 @@ interface OrderClientProps{
 
 const OrderClient = ({data}:OrderClientProps) => {
 
+    const router = useRouter()
+
   return (
-    <>
-      <div className="flex items-center justify-between">
-        <Heading
-          title={`Order(${data.length})`}
-          description="Manage Orders fro your store"
-        />
-
-        <Button
-          onClick={() => router.push(`/${params.storeId}/orders/new`)}
-        >
-          <Plus className="mr-2 h-4 w-4" />
-          Add New
-        </Button>
-      </div>
-      <Separator />
-        <DataTable columns={columns} data={data} searchKey='products'/>
-
-
-
-    </>
+      <>
+          <Heading title={`Orders (${data.length})`} description="Manage orders for your store" />
+          <Separator />
+          <DataTable searchKey="products" columns={columns} data={data} />
+      </>
   );
 };
 
